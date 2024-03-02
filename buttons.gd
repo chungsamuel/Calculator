002d56d026f3042385100ce88681b4535eabfc41
chungsamuel <samuelqchung@gmail.com>
2024-03-02 04:05:35 -0800

    Basic calc func done
    Added in button for 0 and parentheses
    Fixed code to take in multiple digits

--- buttons.gd
+++ buttons.gd
@@ -4,6 +4,7 @@ var numbertest
 var operator_array = []
 var clear: bool = true
 var test_array = [1,2,3]
+var test_string: String = "12 + 3 * 4 - 6"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,14 @@ func _ready():
 	#print(test_array.pop_back())
 	#print(test_array.back())
 	#print(test_array)
+	#var thing = test_string.rsplit(" ")
+	#print(thing)
+	#print(thing[1])
+	#print(thing[2])
+	#print(thing[3])
+	#print(thing[4])
+	#print(thing[5])
+	#print(thing[6])
 	pass
 
 
@@ -34,21 +43,23 @@ func get_priority(c):
 
 
 func convert_to_postfix(infix: String):
+	var string_stack = infix.rsplit(" ")
+	print(string_stack)
 	var operator_stack = []
 	var postfix: String = ""
 	var index: int = 0
 	
-	while infix.length() != index:
-		var next_character = infix[index]
+	while string_stack.size() != index:
+		var next_character = string_stack[index]
 		if next_character.is_valid_int():
-			postfix = postfix + next_character
+			postfix = postfix + " " + next_character
 			index += 1
 			print("here 1")
 		else:
 			match next_character:
 				"+","-","*","/":
 					while !operator_stack.is_empty() && get_priority(next_character) <= get_priority(operator_stack.back()):
-						postfix = postfix + operator_stack.back()
+						postfix = postfix + " " + operator_stack.back()
 						operator_stack.pop_back()
 						print("here 2")
 					operator_stack.push_back(next_character)
@@ -62,7 +73,7 @@ func convert_to_postfix(infix: String):
 				")":
 					var top_operator = operator_stack.pop_back()
 					while top_operator != "(":
-						postfix = postfix + top_operator
+						postfix = postfix + " " + top_operator
 						top_operator = operator_stack.pop_back()
 						index += 1
 					#break
@@ -72,16 +83,17 @@ func convert_to_postfix(infix: String):
 					#break
 	while !operator_stack.is_empty():
 		var top_operator = operator_stack.pop_back()
-		postfix = postfix + top_operator
+		postfix = postfix + " " + top_operator
 		print("here 5")
 	return postfix
 
 func evaluate_postfix(postfix: String):
+	var string_stack = postfix.rsplit(" ")
 	var value_stack = []
 	var index: int = 0
 	
-	while postfix.length() != index:
-		var next_character = postfix[index]
+	while string_stack.size() != index:
+		var next_character = string_stack[index]
 		if next_character.is_valid_int():
 			value_stack.push_back(next_character)
 			index += 1
@@ -118,15 +130,30 @@ func evaluate_postfix(postfix: String):
 
 
 func process_numbers(number):
-	if clear:
-		clear = false
-		$input.text = number
-		print(number)
-	else:
-		$input.text = $input.get_text() + number
-		$input.show()
-		print(number)
-	
+	match number:
+		"(",")":
+			if clear:
+				clear = false
+				$input.text = number
+				print(number)
+			else:
+				$input.text = $input.get_text() + " " + number + " "
+				$input.show()
+				print(number)
+		"+","-","*","/":
+			$input.text = $input.get_text() + " " + number + " "
+			$input.show()
+			print(number)
+		"1", "2", "3", "4", "5", "6", "7", "8", "9", "0":
+			if clear:
+				clear = false
+				$input.text = number
+				print(number)
+			else:
+				$input.text = $input.get_text() + number
+				$input.show()
+				print(number)
+
 
 func _on_number_1_pressed():
 	process_numbers("1")
@@ -202,3 +229,15 @@ func _on_clear_pressed():
 	$input.text = "0"
 	$input.show()
 	clear = true
+
+
+func _on_number_0_pressed():
+	process_numbers("0")
+
+
+func _on_leftparenoperator_pressed():
+	process_numbers("(")
+
+
+func _on_rightparenopeator_pressed():
+	process_numbers(")")
